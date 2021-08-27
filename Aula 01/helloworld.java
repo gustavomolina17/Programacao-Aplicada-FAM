07d738c3ecab351f709928d569ecedaf114d434c
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-08-27 10:01:41 -0300

    Update de exercício

---
+++ Aula 01/helloworld.java
@@ -0,0 +1,21 @@
+// Comentário de uma única linha
+
+/* 
+Esse é o nosso 1º programa java.
+Ele fará a impressão de uma mensagem na tela.
+*/
+
+public class helloworld{
+
+// Método main: Inicia a execução da nossa aplicação java. Possui um padrão
+  
+  
+  public static void main(String args[]){
+  
+    System.out.println("Esse é o meu 1º programa em Java"); //println -> nova linha
+    System.out.println("Hoje é sexta-feira");
+    System.out.println("Vai São Paulo");
+  
+  } // fim do método main
+  
+  } // fim da minha classe

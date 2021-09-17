1a43d6c8902c45b07827e798a976d35159da5e87
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-17 11:52:25 -0300

    Update do scanner

--- Aula 04/exec001.java
+++ Aula 04/exec001.java
@@ -4,13 +4,16 @@ public class exec001 {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         int anoAtual, anoNascimento, qtdeAnos;
         System.out.println("Entre com o ano atual:");
-        anoAtual = new Scanner(System.in).nextInt();
+        anoAtual = sc.nextInt();
         System.out.println("Entre com o ano de nascimento:");
-        anoNascimento = new Scanner(System.in).nextInt();
+        anoNascimento = sc.nextInt();
         qtdeAnos = anoAtual - anoNascimento;
         System.out.println("Em " + anoAtual + " você faz (ou já fez) " + qtdeAnos + " anos!");
+        sc.close();
     }
 
 }

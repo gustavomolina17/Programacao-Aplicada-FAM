8565d6a0fc950864480b8bae63b9123f4445b615
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-17 08:56:40 -0300

    Adicionando o exemplo 01

---
+++ Aula 04/ex01.java
@@ -0,0 +1,33 @@
+/* Aula 04 - Estruturas de Decisão
+
+If -> Se
+
+if(condição){
+
+// código a ser executado
+
+}
+
+*/
+
+//Programa para demonstrar o uso da declaração if
+
+public class ex01 {
+
+    public static void main(String[] args) {
+
+        // definindo uma variável chamada idade
+
+        int idade = 20;
+
+        // checando a idade
+
+        if (idade > 18) {
+
+            System.out.println("Idade maior que 18");
+
+        }
+
+    }
+
+}

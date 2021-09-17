5a6f0f82d843195512370011ac64f0cede3e7e96
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-17 11:56:02 -0300

    Adicionando exercício 3

---
+++ Aula 04/exec003.java
@@ -0,0 +1,15 @@
+//Crie um programa que calcule a média salarial de 4 funcionários da empresa XPTO
+
+public class exec003 {
+
+    public static void main(String[] args) {
+
+        double n1, n2, n3, n4, media;
+        n1 = 3453.21;
+        n2 = 3498.43;
+        n3 = 7902.09;
+        n4 = 12932.00;
+        media = (n1 + n2 + n3 + n4) / 4;
+        System.out.println("A média salarial da empresa é de R$ " + media);
+    }
+}

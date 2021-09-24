f77791d74b7fd5e678bc98026124b1ae3a07d385
Gustavo Molina <gustavo.molina17@yahoo.com.br>
2021-09-24 08:54:25 -0300

    Adicionando Exemplo de While

---
+++ Aula 05/ExemploWhile.java
@@ -0,0 +1,25 @@
+/* While
+
+A estrutura while executa um bloco de instruções enquanto uma determinada condição for verdadeira (true).
+
+while (condição){
+
+<instruções>
+
+}
+
+*/
+
+public class ExemploWhile {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i <= 10) { // Executar o print 10 x
+            System.out.println(++i);
+
+        }
+
+    }
+
+}

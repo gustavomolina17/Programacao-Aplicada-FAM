db150fecbf89b01526b6737dbd06673601a72a15
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-10-01 09:45:19 -0300

    Criando a classe herói

---
+++ Aula 06/heroi.java
@@ -0,0 +1,29 @@
+
+// Criação da classe (Modelo)
+
+public class heroi {
+
+    // Criação dos atributos (Propriedades/ Características do Objeto)
+
+    String nome;
+    int idade;
+    String armadura;
+    float peso;
+    float altura;
+
+    // Métodos (Ações do Objeto)
+    // Método void (não tem nenhum retorno numérico)
+
+    void salvar() {
+        System.out.println("O herói está salvando as vítimas.");
+    }
+
+    void lutar() {
+        System.out.println("O herói está lutando.");
+    }
+
+    void correr() {
+        System.out.println("O heróis está correndo");
+    }
+
+}

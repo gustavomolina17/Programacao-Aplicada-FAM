16e6fe82620205470a65206c74898717d62b64eb
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-10-20 10:19:49 -0300

    Adicionando a classe funcionario

---
+++ Aula 09/Funcionario.java
@@ -0,0 +1,9 @@
+
+public class Funcionario extends Pessoa {
+    public Funcionario(String _nome, String _cpf) {
+        super(_nome, _cpf);
+    }
+
+    public double salario;
+    public String cargo;
+}

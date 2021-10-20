57bfb80ce5e2d40239cf8221eec9f1035d97f9ca
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-10-20 10:20:05 -0300

    Adicionando a classe Professor

---
+++ Aula 09/Professor.java
@@ -0,0 +1,8 @@
+public class Professor extends Pessoa {
+    public Professor(String _nome, String _cpf) {
+        super(_nome, _cpf);
+    }
+
+    public double salario;
+    public String disciplina;
+}

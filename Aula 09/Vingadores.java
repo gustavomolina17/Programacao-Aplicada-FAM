b5d19a3e133a418f731c29562bd090a3ab18344e
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-10-22 10:02:14 -0300

    Criação da classe Vingadores

---
+++ Aula 09/Vingadores.java
@@ -0,0 +1,18 @@
+// Criação da classe base (que será utilizada na herança)
+
+public class Vingadores {
+    public String nome;
+    public int idade;
+    public boolean armadura;
+    public double altura;
+
+    // Método Construtor
+
+    public Vingadores(String nome, int idade, boolean armadura, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.armadura = true;
+        this.altura = altura;
+    }
+
+}

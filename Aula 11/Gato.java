47fa3d7c33d205186bf3a69b2e656bfb03b0d70d
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-03 10:47:39 -0300

    Adicionando a classe gato

---
+++ Aula 11/Gato.java
@@ -0,0 +1,11 @@
+public class Gato extends Animal {
+    public Gato() {
+        this.nome = "Gato";
+        this.numeroPatas = 4;
+    }
+
+    @Override // reescrita do método
+    public void som() {
+        System.out.println("O" + this.nome + " que tem " + this.numeroPatas + " patas, faz MIAU!!!");
+    }
+}

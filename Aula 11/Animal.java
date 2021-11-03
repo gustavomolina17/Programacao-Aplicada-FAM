87be4b66b813692a577d05cae9b0e3831bb73afd
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-03 10:37:17 -0300

    Criação da classe abstrata animal

---
+++ Aula 11/Animal.java
@@ -0,0 +1,13 @@
+// Criação da classe abstrata Animal
+// Abstract -> Operador que indica que a classe é abstrata
+
+public abstract class Animal {
+
+    protected String nome;
+    protected int numeroPatas;
+
+    // Método abstrato
+
+    public abstract void som();
+
+}

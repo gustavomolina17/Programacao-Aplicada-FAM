489a99b112b42042b6e4dd113cc8ca79773bf0f1
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-03 10:54:29 -0300

    Adicionando a classe de teste Bichos

---
+++ Aula 11/Bichos.java
@@ -0,0 +1,17 @@
+// Classe de teste, onde os animais serão instanciados (criados)
+
+public class Bichos {
+
+    public static void main(String[] args) {
+
+        Vaca SaraLee = new Vaca();
+        Gato cookie = new Gato();
+        Carneiro barnabe = new Carneiro();
+
+        SaraLee.som();
+        cookie.som();
+        barnabe.som();
+
+    }
+
+}

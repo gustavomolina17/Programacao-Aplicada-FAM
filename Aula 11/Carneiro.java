e7c99ee71f5de83a12eabe80b5887a68e47a02d0
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-03 10:49:49 -0300

    Adicionando a classe Carneiro

---
+++ Aula 11/Carneiro.java
@@ -0,0 +1,11 @@
+public class Carneiro extends Animal {
+    public Carneiro() {
+        this.nome = "Carneiro";
+        this.numeroPatas = 4;
+    }
+
+    @Override // reescrita do método
+    public void som() {
+        System.out.println("O" + this.nome + " que tem " + this.numeroPatas + " patas, faz BÉÉÉÉ!!!");
+    }
+}

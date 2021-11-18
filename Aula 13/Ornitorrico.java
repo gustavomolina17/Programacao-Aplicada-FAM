9d68fd74e7a9362caf812cf30d3ae78d3ce80411
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-18 16:51:32 -0300

    Criação da Classe Ornitorrinco

---
+++ Aula 13/Ornitorrico.java
@@ -0,0 +1,13 @@
+public class Ornitorrico implements Animal{
+
+public void nasce(){
+  System.out.println("O ornitorrinco nasce de um ovo...");
+}
+
+public void movimenta(){
+  System.out.println("Anda de um lado para o outro...");
+}
+
+public void dorme(){
+  System.out.println("E dorme durante o dia inteiro dentro de tunéis!");
+}}

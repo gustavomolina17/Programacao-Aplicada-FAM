47d5f56ea93c1e36049704cf4fd2124db3be4674
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-18 16:51:02 -0300

    Criação da Classe Morcego

---
+++ Aula 13/Morcego.java
@@ -0,0 +1,13 @@
+public class Morcego implements Animal{
+
+public void nasce(){
+  System.out.println("Nasce um morcego...");
+}
+
+public void movimenta(){
+  System.out.println("Ele voa de lá pra cá...");
+}
+
+public void dorme(){
+  System.out.println("E dorme de cabeça para baixo!");
+}}

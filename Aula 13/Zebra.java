d7c335d07b5436e8f0bc81ab5fcbc6d55085e77e
Gustavo Molina <70485830+gustavomolina17@users.noreply.github.com>
2021-11-18 16:52:04 -0300

    Criação da Classe zebra

---
+++ Aula 13/Zebra.java
@@ -0,0 +1,15 @@
+public class Zebra implements Animal{
+
+ 
+public void nasce(){
+  System.out.println("A zebra nasce cheia de listras...");
+}
+
+public void movimenta(){
+  System.out.println("Fica galopando pelo campo...");
+}
+
+public void dorme(){
+  System.out.println("E dorme em pé!");
+
+}}
